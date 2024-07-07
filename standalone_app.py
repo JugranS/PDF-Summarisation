78fa9a553c8f62e21c3f7454de75d3cc946c851d
Bala Dutt <baladutt@stripe.com>
2024-07-07 14:32:53 +0530

    refactor to make it work in different envrionments

---
+++ standalone_app.py
@@ -0,0 +1,30 @@
+from summarise import summarise
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+
+def load_openai_api_key():
+    dotenv_path = ".env"
+    load_dotenv(dotenv_path)
+    openai_api_key = os.getenv("OpenAI_API_KEY")
+    if not openai_api_key:
+        raise ValueError(f"Unable to retrieve OPENAI_API_KEY from {dotenv_path}")
+    return openai_api_key
+
+def main():
+    try:
+        os.environ["OPENAI_API_KEY"] = load_openai_api_key()
+    except ValueError as e:
+        st.error(str(e))
+        return
+    embeddings = HuggingFaceEmbeddings(model_name='allenai/longformer-base-4096')
+    OpenAIModel = "gpt-3.5-turbo"
+    llm = ChatOpenAI(model=OpenAIModel, temperature=0.5)
+    summarise(llm, embeddings)
+
+if __name__ == '__main__':
+    main()
